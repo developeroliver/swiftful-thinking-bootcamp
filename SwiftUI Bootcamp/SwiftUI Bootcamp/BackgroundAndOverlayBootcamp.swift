80588c5e708ee25382d94943bd7ffc2b72b6ecf3
developpeur.olive as Oliver <92441827+developeroliver@users.noreply.github.com>
2024-05-06 16:45:54 +0200

    SwiftUI Bootcamp overlay

---
+++ SwiftUI Bootcamp/SwiftUI Bootcamp/BackgroundAndOverlayBootcamp.swift
@@ -0,0 +1,42 @@
+//
+//  BackgroundAndOverlayBootcamp.swift
+//  SwiftUI Bootcamp
+//
+//  Created by olivier geiger on 06/05/2024.
+//
+
+import SwiftUI
+
+struct BackgroundAndOverlayBootcamp: View {
+    var body: some View {
+        Image(systemName: "heart.fill")
+                    .font(.system(size: 40))
+                    .foregroundColor(Color.white)
+                    .background(
+                        Circle()
+                            .fill(
+                                LinearGradient(
+                                    gradient: Gradient(colors: [Color(#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)), Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1))]),
+                                    startPoint: .topLeading,
+                                    endPoint: .bottomTrailing)
+                            )
+                            .frame(width: 100, height: 100)
+                            .shadow(color: Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 0.5)), radius: 10, x: 0.0, y: 10)
+                            .overlay(
+                                Circle()
+                                    .fill(Color.blue)
+                                    .frame(width: 35, height: 35)
+                                    .overlay(
+                                        Text("5")
+                                            .font(.headline)
+                                            .foregroundColor(.white)
+                                    )
+                                    .shadow(color: Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 0.5)), radius: 10, x: 5, y: 5)
+                                , alignment: .bottomTrailing)
+                    )b
+    }
+}
+
+#Preview {
+    BackgroundAndOverlayBootcamp()
+}
