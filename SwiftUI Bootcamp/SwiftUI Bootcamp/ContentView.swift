61fc63d201d88d129cd895285dc1b87f1bf3cf73
developpeur.olive as Oliver <92441827+developeroliver@users.noreply.github.com>
2024-05-06 15:04:00 +0200

    SwiftUI-Bootcamp - init project

---
+++ SwiftUI Bootcamp/SwiftUI Bootcamp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI Bootcamp
+//
+//  Created by olivier geiger on 06/05/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI Bootcamp/SwiftUI Bootcamp/SwiftUI_BootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_BootcampApp.swift
+//  SwiftUI Bootcamp
+//
+//  Created by olivier geiger on 06/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_BootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
