99081e53ad7caca09e4ea266443452d0af774450
developpeur.olive as Oliver <92441827+developeroliver@users.noreply.github.com>
2024-05-08 09:37:41 +0200

    SwiftUI Bootcamp - scrollview

---
+++ SwiftUI Bootcamp/SwiftUI Bootcamp/ScrollViewBootcamp.swift
@@ -0,0 +1,35 @@
+//
+//  ScrollViewBootcamp.swift
+//  SwiftUI Bootcamp
+//
+//  Created by olivier geiger on 08/05/2024.
+//
+
+import SwiftUI
+
+struct ScrollViewBootcamp: View {
+    var body: some View {
+        ScrollView {
+            LazyVStack {
+                ForEach(0..<100) { index in
+                    ScrollView(.horizontal, showsIndicators: false, content: {
+                        LazyHStack {
+                            ForEach(0..<20) { index in
+                                RoundedRectangle(cornerRadius: 25.0)
+                                    .fill(Color.white)
+                                    .frame(width: 200, height: 150)
+                                    .shadow(radius: 10)
+                                    .padding()
+                            }
+                        }
+                    })
+                }
+            }
+            .ignoresSafeArea()
+        }
+    }
+}
+
+#Preview {
+    ScrollViewBootcamp()
+}
