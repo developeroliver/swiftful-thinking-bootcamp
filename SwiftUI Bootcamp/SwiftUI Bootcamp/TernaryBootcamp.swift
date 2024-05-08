106b528d0d1b6156d4e2bf2bd7e754541306ff78
developpeur.olive as Oliver <92441827+developeroliver@users.noreply.github.com>
2024-05-08 10:51:52 +0200

    SwiftUI Bootcamp - ternary

---
+++ SwiftUI Bootcamp/SwiftUI Bootcamp/TernaryBootcamp.swift
@@ -0,0 +1,35 @@
+//
+//  TernaryBootcamp.swift
+//  SwiftUI Bootcamp
+//
+//  Created by olivier geiger on 08/05/2024.
+//
+
+import SwiftUI
+
+struct TernaryBootcamp: View {
+    
+    @State var isStartingState: Bool = false
+    
+    var body: some View {
+        VStack {
+            Button("Button: \(isStartingState.description)") {
+                isStartingState.toggle()
+            }
+            
+            Text(isStartingState ? "STARTING STATE!!!" : "ENDING STATE.")
+
+            RoundedRectangle(cornerRadius: isStartingState ? 25 : 0)
+                .fill(isStartingState ? Color.red : Color.blue)
+                .frame(
+                    width: isStartingState ? 200 : 50,
+                    height: isStartingState ? 400 : 50)
+            
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    TernaryBootcamp()
+}
