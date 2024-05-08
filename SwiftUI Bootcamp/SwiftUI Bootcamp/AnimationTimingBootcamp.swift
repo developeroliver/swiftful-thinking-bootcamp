d6911626bd12a60ab74664acd2a4f185a6d33f27
developpeur.olive as Oliver <92441827+developeroliver@users.noreply.github.com>
2024-05-08 11:14:39 +0200

    SwiftUI Bootcamp - animation curves and transitions

---
+++ SwiftUI Bootcamp/SwiftUI Bootcamp/AnimationTimingBootcamp.swift
@@ -0,0 +1,46 @@
+//
+//  AnimationTimingBootcamp.swift
+//  SwiftUI Bootcamp
+//
+//  Created by olivier geiger on 08/05/2024.
+//
+
+import SwiftUI
+
+struct AnimationTimingBootcamp: View {
+    @State var isAnimating: Bool = false
+        let timing: Double = 10.0
+        
+        var body: some View {
+            VStack {
+                Button("Button") {
+                    isAnimating.toggle()
+                }
+                RoundedRectangle(cornerRadius: 20)
+                    .frame(width: isAnimating ? 350 : 50, height: 100)
+                    .animation(.spring(
+                                response: 0.5,
+                                dampingFraction: 0.7,
+                                blendDuration: 1.0))
+                    //.animation(.spring())
+                    //.animation(Animation.linear(duration: timing))
+                
+    //            RoundedRectangle(cornerRadius: 20)
+    //                .frame(width: isAnimating ? 350 : 50, height: 100)
+    //                .animation(Animation.easeIn(duration: timing))
+    //
+    //            RoundedRectangle(cornerRadius: 20)
+    //                .frame(width: isAnimating ? 350 : 50, height: 100)
+    //                .animation(Animation.easeInOut(duration: timing))
+    //
+    //            RoundedRectangle(cornerRadius: 20)
+    //                .frame(width: isAnimating ? 350 : 50, height: 100)
+    //                .animation(Animation.easeOut(duration: timing))
+
+            }
+        }
+}
+
+#Preview {
+    AnimationTimingBootcamp()
+}
