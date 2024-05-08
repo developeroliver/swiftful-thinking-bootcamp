eaec622cd33234dc49295f8f9a1bd5b80c0a05c0
developpeur.olive as Oliver <92441827+developeroliver@users.noreply.github.com>
2024-05-08 09:17:28 +0200

    SwiftUI Bootcamp - inits and enum

---
+++ SwiftUI Bootcamp/SwiftUI Bootcamp/InitializerBootcamp.swift
@@ -0,0 +1,54 @@
+//
+//  InitializerBootcamp.swift
+//  SwiftUI Bootcamp
+//
+//  Created by olivier geiger on 08/05/2024.
+//
+
+import SwiftUI
+
+struct InitializerBootcamp: View {
+    let backgroundColor: Color
+        let count: Int
+        let title: String
+        
+        init(count: Int, fruit: Fruit) {
+            self.count = count
+            
+            if fruit == .apple {
+                self.title = "Apples"
+                self.backgroundColor = .red
+            } else {
+                self.title = "Oranges"
+                self.backgroundColor = .orange
+            }
+        }
+        
+        enum Fruit {
+            case apple
+            case orange
+        }
+        
+        var body: some View {
+            VStack(spacing: 12) {
+                Text("\(count)")
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+                    .underline()
+                
+                Text(title)
+                    .font(.headline)
+                    .foregroundColor(.white)
+            }
+            .frame(width: 150, height: 150)
+            .background(backgroundColor)
+            .cornerRadius(10)
+        }
+}
+
+#Preview {
+    HStack {
+        InitializerBootcamp(count: 5, fruit: .apples)
+        InitializerBootcamp(count: 50, fruit: .orange)
+    }
+}
