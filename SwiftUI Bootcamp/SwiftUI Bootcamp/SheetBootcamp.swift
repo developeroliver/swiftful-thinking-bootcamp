1dd4dcedf4f28d2d26bcd64ad430bd2988bf4988
developpeur.olive as Oliver <92441827+developeroliver@users.noreply.github.com>
2024-05-08 11:32:17 +0200

    SwiftUI Bootcamp - sheet

---
+++ SwiftUI Bootcamp/SwiftUI Bootcamp/SheetBootcamp.swift
@@ -0,0 +1,61 @@
+//
+//  SheetBootcamp.swift
+//  SwiftUI Bootcamp
+//
+//  Created by olivier geiger on 08/05/2024.
+//
+
+import SwiftUI
+
+struct SheetBootcamp: View {
+    @State var showSheet: Bool = false
+        
+        var body: some View {
+            ZStack {
+                Color.green
+                    .edgesIgnoringSafeArea(.all)
+                
+                Button(action: {
+                    showSheet.toggle()
+                }, label: {
+                    Text("Button")
+                        .foregroundColor(.green)
+                        .font(.headline)
+                        .padding(20)
+                        .background(Color.white.cornerRadius(10))
+                })
+    //            .fullScreenCover(isPresented: $showSheet, content: {
+    //                SecondScreen()
+    //            })
+                .sheet(isPresented: $showSheet, content: {
+                    // DO NOT ADD CONDITIONAL LOGIC
+                    SecondScreen()
+                })
+            }
+        }
+    }
+
+    struct SecondScreen: View {
+        
+        @Environment(\.presentationMode) var presentationMode
+        
+        var body: some View {
+            ZStack(alignment: .topLeading) {
+                Color.red
+                    .edgesIgnoringSafeArea(.all)
+                
+                Button(action: {
+                    presentationMode.wrappedValue.dismiss()
+                }, label: {
+                    Image(systemName: "xmark")
+                        .foregroundColor(.white)
+                        .font(.largeTitle)
+                        .padding(20)
+                })
+            }
+        }
+    }
+
+#Preview {
+    SheetBootcamp()
+}
