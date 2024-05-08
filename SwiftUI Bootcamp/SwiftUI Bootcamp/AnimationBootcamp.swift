29ddec5d45f422933f7a930498663378493254f3
developpeur.olive as Oliver <92441827+developeroliver@users.noreply.github.com>
2024-05-08 11:12:48 +0200

    SwiftUI Bootcamp - animation

---
+++ SwiftUI Bootcamp/SwiftUI Bootcamp/AnimationBootcamp.swift
@@ -0,0 +1,41 @@
+//
+//  AnimationsBootcamp.swift
+//  SwiftUI Bootcamp
+//
+//  Created by olivier geiger on 08/05/2024.
+//
+
+import SwiftUI
+
+@available(*, introduced: 13.0, deprecated: 15.0, message: "Use AnimationUpdatedBootcamp instead.")
+
+struct AnimationBootcamp: View {
+    
+    @State var isAnimated: Bool = false
+    
+    var body: some View {
+        VStack {
+            Button("Button") {
+                isAnimated.toggle()
+            }
+            Spacer()
+            RoundedRectangle(cornerRadius: isAnimated ? 50 : 25)
+                .fill(isAnimated ? Color.red : Color.green)
+                .animation(Animation
+                            .default
+                            .repeatForever(autoreverses: true))
+                .frame(
+                    width: isAnimated ? 100 : 300,
+                    height: isAnimated ? 100 : 300)
+                .rotationEffect(Angle(degrees: isAnimated ? 360 : 0))
+                .offset(y: isAnimated ? 300 : 0)
+
+            
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    AnimationBootcamp()
+}
