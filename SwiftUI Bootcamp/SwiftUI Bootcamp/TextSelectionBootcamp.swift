9859d7ca146df2d4ec652d383953dbc6e29d7bd8
developpeur.olive as Oliver <92441827+developeroliver@users.noreply.github.com>
2024-05-10 15:57:18 +0200

    SwiftUI Bootcamp - text selection

---
+++ SwiftUI Bootcamp/SwiftUI Bootcamp/TextSelectionBootcamp.swift
@@ -0,0 +1,19 @@
+//
+//  TextSelectionBootcamp.swift
+//  SwiftUI Bootcamp
+//
+//  Created by olivier geiger on 10/05/2024.
+//
+
+import SwiftUI
+
+struct TextSelectionBootcamp: View {
+    var body: some View {
+        Text("Hello, World!")
+            .textSelection(.enabled)
+    }
+}
+
+#Preview {
+    TextSelectionBootcamp()
+}
