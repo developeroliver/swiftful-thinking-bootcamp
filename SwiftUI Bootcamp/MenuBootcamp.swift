ca9674d1cd3e23bb240d9160402d5875157fe4f8
developpeur.olive as Oliver <92441827+developeroliver@users.noreply.github.com>
2024-05-10 17:41:09 +0200

    SwiftUI Bootcamp - menu

---
+++ SwiftUI Bootcamp/MenuBootcamp.swift
@@ -0,0 +1,31 @@
+//
+//  MenuBootcamp.swift
+//  SwiftUI Bootcamp
+//
+//  Created by olivier geiger on 10/05/2024.
+//
+
+import SwiftUI
+
+struct MenuBootcamp: View {
+    var body: some View {
+           Menu("Click me!") {
+               Button("One") {
+                   
+               }
+               Button("Two") {
+                   
+               }
+               Button("Three") {
+                   
+               }
+               Button("Four") {
+                   
+               }
+           }
+       }
+}
+
+#Preview {
+    MenuBootcamp()
+}
